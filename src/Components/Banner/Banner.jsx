033dd64eea3c319bf77ae2d7431548c3c51fb370
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2023-09-13 09:28:50 +0200

    force font family

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -99,7 +99,7 @@ export function Banner({ children }) {
 	const { classes } = useStyles();
 	return (
 		<div className={classes.root}>
-			<Container size="1200px">
+			<Container size="lg">
 				<Paper>
 					<Navbar>
 						{children}
